de75d1e172b42c49b4040dcfbbe0e5e5817dbc89
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-04-17 23:43:34 +0700

    commit countdown date time

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,3 +36,39 @@ navs.forEach((nav,i) => {
           currentSlide = i;
      });
 });
+
+// Count down
+
+const countDown = () =>{
+
+     //Đặt ngày đếm ngược
+     var countDate = new Date ("september 13 , 2022 00:00:00 ").getTime();
+
+
+     //Lấy ngày và giờ hôm nay
+     var now = new Date().getTime();
+     //Tìm khoảng cách giữa ngày bây giờ và giờ đếm ngược
+     var distance = countDate - now;
+   
+    // 
+     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+     //Kiểm tra
+     days = (days < 100) ? '00' + days : days;
+     hours = (hours < 10) ? '0' + hours : hours;
+     minutes = (minutes < 10) ? '0' + minutes : minutes;
+     seconds = (seconds < 10) ? '0' + seconds : seconds;
+   
+     // Hiển thị
+     document.querySelector("#days").innerHTML = days;
+     document.querySelector("#hours").innerHTML = hours;
+     document.querySelector("#minutes").innerHTML = minutes;
+     document.querySelector("#seconds").innerHTML = seconds;
+     
+     
+};
+
+setInterval(countDown, 1000);
