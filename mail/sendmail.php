06b4bc0437b106f910c9fa86491ad16589910919
HOANGNGOCTHUY <hoangngocthuy6@gmail.com>
2022-05-16 23:32:02 +0700

    email

---
+++ mail/sendmail.php
@@ -0,0 +1,11 @@
+<?php
+include APPPATH . "PHPMailer/src/PHPMailer.php";
+include APPPATH . "PHPMailer/src/Exception.php";
+include APPPATH . "PHPMailer/src/OAuth.php";
+include APPPATH . "PHPMailer/src/POP3.php";
+include APPPATH . "PHPMailer/src/SMTP.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+?>
